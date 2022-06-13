0b8cd018c269cc93031965fe3d6c34e3a4068845
Евгений Евгений <asshcm@mail.ru>
2022-06-13 18:59:42 +0300

    Добавьте файлы проекта.

---
+++ Неделя 6/Неделя 6.cpp
@@ -0,0 +1,36 @@
+﻿// Неделя 6.cpp : Этот файл содержит функцию "main". Здесь начинается и заканчивается выполнение программы.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	
+		//Задача 1
+	/*int a, summ = 0;
+	cout << "Введите число от 0 до 500" << endl;
+	cin >> a;
+	while (a <= 500) {
+		summ += a;
+		a++;
+	}
+	cout << "Сумма чисел равна " << summ;*/
+
+	// Задача 2
+
+	int x, y, i = 1;
+	cout << "Введите число Х" << endl;
+	cin >> x;
+	cout << "Введите число Y" << endl;
+	cin >> y;
+	while (i < y) {
+
+	}
+	{
+
+	}
+
+}
+
