3d75d6da263d303af6bf28240d703699c9efc2bf
Евгений Евгений <asshcm@mail.ru>
2022-06-14 20:45:16 +0300

    Закончил ДЗ 6

--- Неделя 6/Неделя 6.cpp
+++ Неделя 6/Неделя 6.cpp
@@ -20,16 +20,62 @@ int main()
 
 	// Задача 2
 
-	int x, y, i = 1;
+	/*int x, y, i = 1;
+	float rezul = 1;
 	cout << "Введите число Х" << endl;
 	cin >> x;
 	cout << "Введите число Y" << endl;
 	cin >> y;
-	while (i < y) {
-
+	if (y != 0) {
+		while (i <= abs(y)) {
+			rezul = rezul * x;
+			i++;
+		}
 	}
-	{
+	cout << "Результа равен  " << rezul;
+	
+	*/
 
+	
+			// Задача 3
+
+	/*int i;
+	float rezul = 0;
+	for (i = 1; i <= 1000; i++) {
+		rezul += i;
+	}
+	cout << "" << rezul / 1000;
+	
+	*/
+
+
+	// Задача 4
+
+	/*int a, rezul = 0;
+	cout << "Введите целое число от 1 до 20" << endl;
+	cin >> a;
+	if (a >= 1 && a <= 20) {
+		for (a; a <= 20; a++) {
+			rezul += a;
+		}
+		cout << "Результат произведения равен  " << rezul;
+	}
+	else
+	{
+		cout << "Введено не верное число";
+	}*/
+
+	int a, i;
+	cout << "Введите номер варианта от 1 до 10" << endl;
+	cin >> a;
+	if (a >= 1 && a <= 10) {
+		for (i = 1; i <= 10; i++) {
+			cout << a << " * " << i << " = " << a * i << endl;
+		}
+	}
+	else
+	{
+		cout << "Введен не верное число варианта";
 	}
 
 }
